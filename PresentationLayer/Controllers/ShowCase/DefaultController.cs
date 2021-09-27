257718d211f286ba14eac4ca5760b8a8324534e9
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-09-27 17:39:35 +0300

    Mvc Proje Kampı 82 Vitrinde Tüm Başlıkları Listeleme

---
+++ PresentationLayer/Controllers/ShowCase/DefaultController.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentationLayer.Controllers.ShowCase
+{
+    public class DefaultController : Controller
+    {
+        HeadingManager headingManager = new HeadingManager(new EfHeadingDal());
+        ContentManager contentManager = new ContentManager(new EfContentDal());
+        // GET: Default
+        public PartialViewResult Index()
+        {
+           var list = contentManager.GetList();
+            return PartialView(list);
+        }
+
+        public ActionResult Headings()
+        {
+            var list = headingManager.GetList();
+            return View(list);
+        }
+    }
+}
