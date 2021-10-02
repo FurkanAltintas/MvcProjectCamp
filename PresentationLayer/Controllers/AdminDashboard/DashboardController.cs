52e8284a48ae8a314898ebdcf9d0d17a6c6d92be
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-02 16:25:30 +0300

    Dashboard Son Mesajlar Eklendi

--- PresentationLayer/Controllers/AdminDashboard/DashboardController.cs
+++ PresentationLayer/Controllers/AdminDashboard/DashboardController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using PresentationLayer.Security;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,12 +9,16 @@ using System.Web.Mvc;
 
 namespace PresentationLayer.Controllers.AdminDashboard
 {
-    public class DashboardController : Controller
+    public class DashboardController : AdminLoginController
     {
         StatisticManager statisticManager = new StatisticManager(new CategoryManager(new EfCategoryDal()),
                                                                  new ContentManager(new EfContentDal()),
                                                                  new HeadingManager(new EfHeadingDal()),
                                                                  new WriterManager(new EfWriterDal()));
+
+        MessageManager messageManager = new MessageManager(new EfMessageDal());
+
+        AdminLoginController adminLoginController = new AdminLoginController(); //id alma
         // GET: Dashboard
         public ActionResult Index()
         {
@@ -23,6 +28,33 @@ namespace PresentationLayer.Controllers.AdminDashboard
             ViewBag.Writer = statisticManager.ListWriter();
 
             return View();
+        }
+
+        public PartialViewResult RecentMessage()
+        {
+            var inbox = messageManager.GetListInboxUnRead(DontHashMail);
+            return PartialView(inbox);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var key = messageManager.GetById(id);
+            key.IsTrash = true;
+            messageManager.Update(key);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Save(int id)
+        {
+            var key = messageManager.GetById(id);
+            key.IsRead = true;
+            messageManager.Update(key);
+            return RedirectToAction("Index");
+        }
+
+        public void GetId(int id)
+        {
+            var key = messageManager.GetById(id);
         }
     }
 }
