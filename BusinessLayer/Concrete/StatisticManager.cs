35ef806e684bdda5140f01eaef79d933b207b86a
Furkan Altıntaş <furkanaltintas785@gmail.com>
2021-10-02 15:43:36 +0300

    Admin Paneli Düzeltmeleri Yapıldı

---
+++ BusinessLayer/Concrete/StatisticManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class StatisticManager : IStatisticService
+    {
+        ICategoryService _categoryService;
+        IContentService _contentService;
+        IHeadingService _headingService;
+        IWriterService _writerService;
+
+        public StatisticManager(ICategoryService categoryService, IContentService contentService, IHeadingService headingService, IWriterService writerService)
+        {
+            _categoryService = categoryService;
+            _contentService = contentService;
+            _headingService = headingService;
+            _writerService = writerService;
+        }
+
+        public int ListCategory()
+        {
+            return _categoryService.GetList().Count();
+        }
+
+        public int ListContent()
+        {
+            return _contentService.GetList().Count();
+        }
+
+        public int ListHeading()
+        {
+            return _headingService.GetList().Count();
+        }
+
+        public int ListWriter()
+        {
+            return _writerService.GetList().Count();
+        }
+    }
+}
--- PresentationLayer/Controllers/AdminDashboard/DashboardController.cs
+++ PresentationLayer/Controllers/AdminDashboard/DashboardController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,18 @@ namespace PresentationLayer.Controllers.AdminDashboard
 {
     public class DashboardController : Controller
     {
+        StatisticManager statisticManager = new StatisticManager(new CategoryManager(new EfCategoryDal()),
+                                                                 new ContentManager(new EfContentDal()),
+                                                                 new HeadingManager(new EfHeadingDal()),
+                                                                 new WriterManager(new EfWriterDal()));
         // GET: Dashboard
         public ActionResult Index()
         {
+            ViewBag.Category = statisticManager.ListCategory();
+            ViewBag.Content = statisticManager.ListContent();
+            ViewBag.Heading = statisticManager.ListHeading();
+            ViewBag.Writer = statisticManager.ListWriter();
+
             return View();
         }
     }
